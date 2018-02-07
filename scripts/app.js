e54587164229d949b4382b45b3037a98a747fff1
febolivar <fe.bolivar@uniandes.edu.co>
2018-02-06 21:37:09 -0500

    Implementación del ServiceWorker
    Implementación del ServiceWorker para el caché de los horarios en las
    estaciones.

--- scripts/app.js
+++ scripts/app.js
@@ -213,6 +213,14 @@
 		app.saveSelectedTimetables();
 	  }	
 	  
+	  
+	// FB: Implementación del ServiceWorker
+    if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+	  
 	
    
 })();
